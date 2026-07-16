3d1dfd5d36f194718cd567c0ce832b43dfd081fe
Grazielly Lacerda <graziilacerda19@gmail.com>
2026-07-16 20:05:11 -0300

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-
-function toggleMenu() {
-    const ul = document.querySelector('ul');
-    if (ul.style.display === "flex") {
-        ul.style.display = "none";
-    } else {
-        ul.style.display = "block";
-    }
-};
